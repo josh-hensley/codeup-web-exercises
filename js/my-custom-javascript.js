efa59052004ff16c3c5c1996ccbe1198056cc3e8
josh-hensley <me@joshhensley.com>
2024-07-30 18:10:58 -0500

    finished mouse events exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,7 +1,19 @@
 "use strict";
 $(function(){
-    $('li').css('font-size', '20px');
-    $('h1, p, li').css('background-color', 'yellow');
-    alert($('h1').html());    
+    function changeColor(){
+        $(this).css('background-color', 'blue');
+    }
+    function changeSize(){
+        $(this).css('font-size', '18px');
+    }
+    function toRed(){
+        $(this).css('color', 'red');
+    }
+    function toBlack(){
+        $(this).css('color', 'black');
+    }
+    $('h1').click(changeColor);
+    $('p').dblclick(changeSize);
+    $('li').hover(toRed, toBlack);
 });
 
