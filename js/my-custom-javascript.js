6c146a845c800e64050273379b57d48885d458ec
josh-hensley <me@joshhensley.com>
2024-07-30 15:43:50 -0500

    finished id selector exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -2,3 +2,9 @@
 $(function(){
     alert('DOM has finished loading');
 });
+
+var mainHeader = $('#main-header').html();
+var infoText = $('#info-text').html();
+
+alert(mainHeader);
+alert(infoText);
