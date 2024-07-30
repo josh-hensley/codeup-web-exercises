fe7b3cb638719e956a7a224c3db26a46bdf23b9d
josh-hensley <me@joshhensley.com>
2024-07-30 17:59:22 -0500

    finished all Selectors exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,7 @@
 "use strict";
 $(function(){
-    $('.codeup').css('border', '1px solid red');    
+    $('li').css('font-size', '20px');
+    $('h1, p, li').css('background-color', 'yellow');
+    alert($('h1').html());    
 });
 
